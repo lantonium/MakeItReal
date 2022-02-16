d8e8d1ba993b4f482a67f67f27540bd120242519
Luis <lantonium@outlook.com>
2022-02-16 11:08:25 -0500

    Create index.js

---
+++ Dia 1/index.js
@@ -0,0 +1,15 @@
+console.log(10); 
+console.log(true);
+console.log(false);
+console.log("Hola mundo!"); 
+console.log('Hola mundo!');
+
+console.log(typeof 10);
+console.log(typeof true); console.log(typeof false);
+console.log(typeof "Hola mundo!");
+console.log(typeof 'Hola mundo!');
+
+console.log(10 + 5* (2 - 3)); 
+console.log(true);
+console.log(false);
+console.log("Hola mundo!");
