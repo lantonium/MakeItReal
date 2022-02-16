efe527b91e10a729465ce97ee3c03345cb71dd6f
Luis <lantonium@outlook.com>
2022-02-16 11:25:59 -0500

    Update index.js

--- Dia 1/index.js
+++ Dia 1/index.js
@@ -5,11 +5,27 @@ console.log("Hola mundo!");
 console.log('Hola mundo!');
 
 console.log(typeof 10);
-console.log(typeof true); console.log(typeof false);
+console.log(typeof true); 
+console.log(typeof false);
 console.log(typeof "Hola mundo!");
 console.log(typeof 'Hola mundo!');
 
-console.log(10 + 5* (2 - 3)); 
-console.log(true);
-console.log(false);
-console.log("Hola mundo!");
+/*
+Operadores aritméticos:
++
+-
+*
+/
+*/
+console.log(10 + 5* (2-3)); // expresión aritmética
+/*
+Operadores relacionales:
+>
+<
+>=
+<=
+==
+===
+*/
+console.log(10> 5); // true
+console.log(2 == 2); // true console.log(3< -1); // false
