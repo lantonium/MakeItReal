1dff99db26db1b7702843cc889ebe90c04e08e27
Luis <lantonium@outlook.com>
2022-02-18 13:20:00 -0500

    Update Dia 1.js

--- Dia 1/Dia 1.js
+++ Dia 1/Dia 1.js
@@ -1,3 +1,4 @@
 console.log(401 == "401") &&  console.log("T" || "F" || "F") && console.log("F" && "V") || "F" || "F" || "T" || (10 < 5) || (404 == "404");
-console.log(14 + 15 ) * 30 - 20 * 10 + (33 + 51)
-
+console.log(14 + 15 ) * 30 - 20 * 10 + (33 + 51);
+console.log(23 + 5 * (2 - 3) > 5 * 10) && console.log(T || F || (10 >= 3));
+console.log(10 <= (5 + 4)) && console.log(100 == "100");
