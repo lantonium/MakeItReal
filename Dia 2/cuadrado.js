37a55e556f9a183549ea6e922cf3c9aa6f1603c8
Luis <lantonium@outlook.com>
2022-02-24 10:33:41 -0500

    Create cuadrado.js

---
+++ Dia 2/cuadrado.js
@@ -0,0 +1,10 @@
+const argumentos = process.argv;
+const numero1 = argumentos[2] * argumentos[2];
+const numero2 = argumentos[3] * argumentos[3];
+const numero3 = argumentos[4] * argumentos[4];
+const numero4 = argumentos[5] * argumentos[5];
+const numero5 = argumentos[6] * argumentos[6];
+
+console.log(numero1, numero2, numero3, numero4, numero5);
+
+const numeros = (numero1, numero2, numero3, numero4, numero5);
