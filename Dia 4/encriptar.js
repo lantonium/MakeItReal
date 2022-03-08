b067fb560590ab0bd9fe2d8b2e3e1d63ad499ddf
Luis <lantonium@outlook.com>
2022-03-08 16:27:50 -0500

    Update encriptar.js

--- Dia 4/encriptar.js
+++ Dia 4/encriptar.js
@@ -26,7 +26,11 @@ function encriptar(texto){
 
 function desencriptar(texto){
   const abecedario = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  let respuesta = "";
+  let respuesta2 = "";
   //dejar aquí su código
-  return respuesta;
+  for(let i = 0; i < texto.length; i++){
+    let posicion = abecedario.indexOf(texto[i]);
+    respuesta = posicion === 0 ? respuesta2 - "Z" : respuesta2 + abecedario[posicion + 1];
+  }
+  return respuesta2;
 }
